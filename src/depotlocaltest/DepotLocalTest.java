f9e35c2249f6140b1baf7deb87af7e2e7858aca6
nicho <nicho@192.168.2.48>
2023-05-22 12:57:08 -0400

    Initial commit

---
+++ src/depotlocaltest/DepotLocalTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package depotlocaltest;
+
+/**
+ *
+ * @author nicho
+ */
+public class DepotLocalTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
